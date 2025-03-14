0d1be762f86d19412b4a475e5546a136a178efdf
AslamtoIbrahim <aslamtoibrahim@gmail.com>
2025-03-14 17:49:55 +0000

    Added responsive design for medium screens

--- src/app/Footer/Footer.tsx
+++ src/app/Footer/Footer.tsx
@@ -4,7 +4,7 @@ import Connection from './Connection'
 
 const Footer = () => {
   return (
-    <div className='foot px-4 py-12'>
+    <div className='foot px-4 py-12 md:py-32'>
       <News />
       <Connection />
     </div>
